d01253ea5f26e04f15355692324356d27e72af7e
Mechanic <yuanxiang.ma@outlook.com>
2019-04-05 23:40:46 +0800

    add cookie pool

---
+++ cookie_pool.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+def get_cookie():  # 获取cookie
+    chrome_obj = webdriver.Chrome()
+    chrome_obj.get("http://weixin.sogou.com/")
+
+    chrome_obj.find_element_by_xpath('//*[@id="loginBtn"]').click()
+    time.sleep(15)
+    cookies = chrome_obj.get_cookies()
+    cookie = {}
+    for items in cookies:
+        cookie[items.get('name')] = items.get('value')
+    return cookie
+
+def get_n_cookies(n):
+    cookie_list = list()
+    for i in range(n):
+        print("第"+i+"个")
+        cookie = get_cookie()
+        cookie_list.append(cookie)
+    return cookie_list
